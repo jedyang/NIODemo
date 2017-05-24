789c343c5e5dac6ceba8242adf8112f200766c75
yunsheng <onlyyunsheng@126.com>
2017-05-24 17:48:03 +0800

    NIOSocket

---
+++ src/test/java/com/yunsheng/ServerSocketChannelDemoTest.java
@@ -0,0 +1,54 @@
+package com.yunsheng;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import org.junit.Test;
+
+/**
+ * Created by shengyun on 17/5/24.
+ */
+public class ServerSocketChannelDemoTest {
+
+    // 创建几个事件
+    @Test
+    public void testSocket() {
+        doSend(9001, "hello 9001");
+        doSend(9002, "hello 9002");
+        doSend(9003, "hello 9003");
+
+    }
+
+    private void doSend(int i, String s) {
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1", i);
+            OutputStream outputStream = socket.getOutputStream();
+            byte[] bytes = s.getBytes();
+            try {
+                outputStream.write(bytes);
+            } finally {
+                outputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test1(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("aa");
+        System.out.println(sb.toString());
+    }
+
+}

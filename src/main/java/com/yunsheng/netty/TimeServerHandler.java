a8e27447f55ce96974512ca37ee33e8bfc69a320
Yang Yunsheng 杨云胜(DTS) <01471076@corp.haier.com>
2017-08-08 11:00:12 +0800

    final

--- src/main/java/com/yunsheng/netty/TimeServerHandler.java
+++ src/main/java/com/yunsheng/netty/TimeServerHandler.java
@@ -13,7 +13,7 @@ public class TimeServerHandler extends ChannelInboundHandlerAdapter {
 
     // channelActive是当连接建立完成开始调用
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    public void channelActive(final ChannelHandlerContext ctx) throws Exception {
         // 4字节是因为我们准备返回一个32位的int，表示时间
         ByteBuf time = ctx.alloc().buffer(4);
         // 2208988800为1900年1月1日00:00:00~1970年1月1日00:00:00的总秒数
@@ -22,7 +22,6 @@ public class TimeServerHandler extends ChannelInboundHandlerAdapter {
 
         final ChannelFuture f = ctx.writeAndFlush(time);
         f.addListener(new ChannelFutureListener() {
-            @Override
             public void operationComplete(ChannelFuture channelFuture) throws Exception {
                 assert f == channelFuture;
                 // 在netty中，所有操作都是异步的。

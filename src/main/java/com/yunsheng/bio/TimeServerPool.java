ce6f7e391ee2933c89f525b789a21b98053fa17c
杨云胜 <yangyunsheng@haier.com>
2018-06-27 09:58:47 +0800

    BIO 使用线程池版本

--- src/main/java/com/yunsheng/bio/TimeServerPool.java
+++ src/main/java/com/yunsheng/bio/TimeServerPool.java
@@ -3,21 +3,25 @@ package com.yunsheng.bio;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * BIO模式的TimeServer
- * 一请求一线程模式
- * 缺点：高并发时，线程暴增，耗尽资源风险
+ * 线程池模式
  */
-public class TimeServer {
+public class TimeServerPool {
     public static void main(String[] args){
         ServerSocket serverSocket = null;
         try {
              serverSocket = new ServerSocket(8000);
             System.out.println("BIO ServerSocket started");
+            ExecutorService executorService = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), 100, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(1000));
             while (true){
                 Socket acceptSocket = serverSocket.accept();
-                new Thread(new TimeServerHandler(acceptSocket)).start();
+                executorService.execute(new TimeServerHandler(acceptSocket));
             }
         } catch (IOException e) {
             e.printStackTrace();
